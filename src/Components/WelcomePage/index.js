1205e97a46874ec452050609195e90757645490a
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-09-05 14:11:30 -0300

    fix: corrigido finalizado

--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -5,7 +5,7 @@ function WelcomePage({ user, setIsLoggedIn }) {
   };
   return (
     <div>
-      <h2>Bem Vindo {user}</h2>
+      <h2>Bem Vindo {user} </h2>
       <button class="Button Button--Logout" onClick={HandleLogout}>
         Logout
       </button>
